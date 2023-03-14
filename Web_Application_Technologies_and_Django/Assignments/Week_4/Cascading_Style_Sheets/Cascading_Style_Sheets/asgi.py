5bc992dfd399657a72ebbd999e1bb410a49ecc44
fadi alahmad <fadi.alahmad.alomar@gmail.com>
2023-03-14 22:02:27 +0200

    Added Week 4's assignment

---
+++ Web_Application_Technologies_and_Django/Assignments/Week_4/Cascading_Style_Sheets/Cascading_Style_Sheets/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Cascading_Style_Sheets project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cascading_Style_Sheets.settings')
+
+application = get_asgi_application()
