33ec067241cbbbb235a5ce565d73d604d7e88144
fadi alahmad <fadi.alahmad.alomar@gmail.com>
2023-03-21 20:38:30 +0200

    Added to ALLOWED_HOSTS

--- Building_Web_Applications_in_Django/Assignments/Week_1/Django_Tutorial_part_2/Django_Tutorial_part_2/settings.py
+++ Building_Web_Applications_in_Django/Assignments/Week_1/Django_Tutorial_part_2/Django_Tutorial_part_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-77+ln*od+r2s(=p_xee)r!=vi=9pe##qmu!a387^v&vhzi@ws8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
